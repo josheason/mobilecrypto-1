947e3e4dc57a691cf2dd32f7397778655bc7b775
josheason <67991218+josheason@users.noreply.github.com>
2020-09-09 19:09:10 -0400

    Update WhatIsDeFi.js

--- pages/WhatIsDeFi.js
+++ pages/WhatIsDeFi.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View, TouchableOpacity, StyleSheet, ScrollView } from 'react-native'
 import { Actions } from 'react-native-router-flux';
-import { Video } from 'expo';
+import { Video } from 'expo-av';
 import { MaterialIcons, Octicons } from '@expo/vector-icons';
 
 //export default 
