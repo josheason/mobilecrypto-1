fc7da71ad3421f6e94bfe9d7bd6c14d5fa2c573a
josheason <67991218+josheason@users.noreply.github.com>
2020-09-09 13:50:13 -0400

    Update WhatIsDeFi.js

--- pages/WhatIsDeFi.js
+++ pages/WhatIsDeFi.js
@@ -1,41 +1,42 @@
 import React, { Component } from 'react'
 import { Text, View, TouchableOpacity, StyleSheet, ScrollView } from 'react-native'
-import { Actions } from 'react-native-router-flux';
 import Video from 'react-native-video';
- 
+import { Actions } from 'react-native-router-flux';
+
+
+export default class Video extends Component {
+ render(){
+  return(
+    <View style={styles.container}>
+      <Video source={{uri: "Url"}}   // Can be a URL or a localfile.
+       ref={(ref) => {
+         this.player = ref
+       }}                                      // Store reference
+       onBuffer={this.onBuffer}                // Callback when remote video is buffering
+       onEnd={this.onEnd}                      // Callback when playback finishes
+       onError={this.videoError}               // Callback when video cannot be loaded
+       style={styles.backgroundVideo} />
+</View>
+);
+}
+}
+const styles = StyleSheet.create({
+  container:{ flex: 1, justifyContent: "center"},
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
+
 // Within your render function, assuming you have a file called
 // "background.mp4" in your project. You can include multiple videos
 // on a single screen if you like.
 
 
 
-export default class DeFi extends React.Component {
-  state={
-  }
-  /*alertItemName = (item) => {
-      alert(item.name)
-   }*/
-   render() {
-      return (
-         <ScrollView>
-            {
-               <Video source={{uri: "background"}}   // Can be a URL or a local file.
-       ref={(ref) => {
-         this.player = ref
-       }}                                      // Store reference
-       onBuffer={this.onBuffer}                // Callback when remote video is buffering
-       onError={this.videoError}               // Callback when video cannot be loaded
-       style={styles.backgroundVideo} />
-            }    
-         </ScrollView>
-         
-      )
-   }
- // navigate(){
-   //     Actions.list()
-    //}
-}
-//export default List
 
 const styles = StyleSheet.create ({
    container: {
