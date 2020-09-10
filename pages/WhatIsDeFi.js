fcf907fd8b303ed6c7bf94eeaab4084fc9ef2832
josheason <67991218+josheason@users.noreply.github.com>
2020-09-10 09:13:38 -0400

    Update WhatIsDeFi.js

--- pages/WhatIsDeFi.js
+++ pages/WhatIsDeFi.js
@@ -1,79 +1,35 @@
 import React, { Component } from 'react'
 import { Text, View, TouchableOpacity, StyleSheet, ScrollView } from 'react-native'
-import { Actions } from 'react-native-router-flux';
 import { Video } from 'expo-av';
-import { MaterialIcons, Octicons } from '@expo/vector-icons';
 
-//export default 
-class List extends Component {
+class WhatIsDeFi extends Component {
 	state = {
-		mute: false,
-		fullScreen: false,
-		shouldPlay: true,
+		test: " "
 	}
-
-	handlePlayAndPause = () => {
-		this.setState(prevState => ({
-			shouldPlay: !prevState.shouldPlay
-		}));
+	render(){ 
+	return(
+		<View>
+		<Video
+  		source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+  		rate={1.0}
+ 		volume={1.0}
+  		isMuted={false}
+  		resizeMode="cover"
+  		shouldPlay
+  		isLooping
+  		style={{ width: 300, height: 300 }}
+		/>
+		</View>
+		)
 	}
-
-	handleVolume = () => {
-		this.setState(prevState => ({
-			mute: !prevState.mute,
-		}));
-	}
-
-  render() {
-		const { width } = Dimensions.get('window');
-		
-    return (
-      <View style={styles.container}>
-				<View>
-						<Text style={{ textAlign: 'center' }}> React Native Video </Text>
-						<Video
-							source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
-							shouldPlay={this.state.shouldPlay}
-							resizeMode="cover"
-							style={{ width, height: 300 }}
-							isMuted={this.state.mute}
-						/>
-						<View style={styles.controlBar}>
-							<MaterialIcons 
-								name={this.state.mute ? "volume-mute" : "volume-up"}
-								size={45} 
-								color="white" 
-								onPress={this.handleVolume} 
-							/>
-							<MaterialIcons 
-								name={this.state.shouldPlay ? "pause" : "play-arrow"} 
-								size={45} 
-								color="white" 
-								onPress={this.handlePlayAndPause} 
-							/>
-						</View>
-					</View>
-      </View>
-		);
-  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-	},
-	controlBar: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-		height: 45,
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: "rgba(0, 0, 0, 0.5)",
-	}
-});
+const styles = StyleSheet.create ({
+   container: {
+     	   flex: 1,
+	   backgroundColor: '#fff',
+	   alignItems: 'center',
+	   justifyContent: 'center',
+   },
+   }
+})
