b07b3402f4b1cc0322c452a0bd62f8c1d39feb18
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 14:15:09 -0400

    Update and rename trademoon.js to trademoon12.js

--- pages/trademoon12.js
+++ pages/trademoon12.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function trademoon(){
+export default function trademoon12(){
 
 	return(
 		<View style = {styles.container}>
