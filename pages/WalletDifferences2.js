796a8ba2ec41876669d31bca464bea67633c6e28
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 14:10:58 -0400

    Update and rename WalletDifferences.js to WalletDifferences2.js

--- pages/WalletDifferences2.js
+++ pages/WalletDifferences2.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function WalletDifferences(){
+export default function WalletDifferences2(){
 
 	return(
 		<View style = {styles.container}>
