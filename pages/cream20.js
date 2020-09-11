2165b5baac4140c9dfac00bab8a67e83e7e71d53
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 14:18:52 -0400

    Update and rename cream.js to cream20.js

--- pages/cream20.js
+++ pages/cream20.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function cream(){
+export default function cream20(){
 
 	return(
 		<View style = {styles.container}>
