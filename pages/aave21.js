fd9710b621fe87eca3ed5e87ed35c53b4d769240
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 14:18:36 -0400

    Update and rename aave.js to aave21.js

--- pages/aave21.js
+++ pages/aave21.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function aave(){
+export default function aave21(){
 
 	return(
 		<View style = {styles.container}>
