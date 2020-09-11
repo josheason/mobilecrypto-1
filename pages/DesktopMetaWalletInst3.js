82b1cc0a27b891f0f447afcb65aac8ca86a38e2e
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 14:11:24 -0400

    Update and rename DesktopMetaWalletInst.js to DesktopMetaWalletInst3.js

--- pages/DesktopMetaWalletInst3.js
+++ pages/DesktopMetaWalletInst3.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function DesktopMetaWalletInst(){
+export default function DesktopMetaWalletInst3(){
 
 	return(
 		<View style = {styles.container}>
