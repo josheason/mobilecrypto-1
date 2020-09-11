c00232b1a84ce9ba78e55cdd25ae99ac99691d90
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 07:27:36 -0400

    Update DeFi.js

--- pages/DeFi.js
+++ pages/DeFi.js
@@ -159,9 +159,9 @@ class DeFi extends Component {
          Actions.etherscan();
       if(itemid == 9)
          Actions.speedmeta();
-      /*
       if(itemid == 10)
-         Actions.Week9();
+         Actions.cancelmeta();
+      /*
       if(itemid == 11)
          Actions.Week10();
       if(itemid == 12)
