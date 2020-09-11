6819437867f2f7865b0effd1b1cfd90340d9cb72
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 14:17:18 -0400

    Update and rename zapper.js to zapper18.js

--- pages/zapper18.js
+++ pages/zapper18.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function zapper(){
+export default function zapper18(){
 
 	return(
 		<View style = {styles.container}>
