ec32de359b818aa4332c0e39a2415167debe743b
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 09:49:07 -0400

    Update Login.js

--- pages/Login.js
+++ pages/Login.js
@@ -1,3 +1,135 @@
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity, StyleSheet, ScrollView, Image  } from 'react-native'
+import { Actions } from 'react-native-router-flux';
+
+
+
+class List extends Component {
+   state = {
+      names: [
+         {
+            id: 0,
+            name: 'Continue To Course',
+         },
+         {
+            id: 1,
+            name: 'FaceBook Group',
+         },
+         {
+            id: 2,
+            name: 'Live Q&A',
+         },
+         {
+            id: 3,
+            name: 'Alex&spos;s Youtube',
+         },
+      ]
+   }
+   alertItemName = (item) => {
+      alert(item.name)
+   }
+   
+   render() {
+      return (
+         <ScrollView
+          style = {{ backgroundColor: '#595959' }}
+         >
+            {
+               this.state.names.map((item, index) => (
+                  if(item.id == 0)
+                  <TouchableOpacity
+                     key = {item.id}
+                     style = {styles.container}
+                     onPress={()=>this.navigate(item.id)}>
+                       <Image 
+                    source={require('./images/continue.png')}  
+                        />
+                     <Text style = {styles.text}>
+                        {item.name}
+                     </Text>
+                  </TouchableOpacity>
+                 if(item.id == 1)
+                   <TouchableOpacity
+                     key = {item.id}
+                     style = {styles.container}
+                     onPress={()=>this.navigate(item.id)}>
+                       <Image 
+                    source={require('./images/facebook.png')}  
+                        />
+                     <Text style = {styles.text}>
+                        {item.name}
+                     </Text>
+                  </TouchableOpacity>
+                 if(item.id == 2)
+                   <TouchableOpacity
+                     key = {item.id}
+                     style = {styles.container}
+                     onPress={()=>this.navigate(item.id)}>
+                       <Image 
+                    source={require('./images/live.png')}  
+                        />
+                     <Text style = {styles.text}>
+                        {item.name}
+                     </Text>
+                  </TouchableOpacity>
+                 if(item.id == 3)
+                   <TouchableOpacity
+                     key = {item.id}
+                     style = {styles.container}
+                     onPress={()=>this.navigate(item.id)}>
+                       <Image 
+                    source={require('./images/youtube.png')}  
+                        />
+                     <Text style = {styles.text}>
+                        {item.name}
+                     </Text>
+                  </TouchableOpacity>
+               ))
+            }    
+         </ScrollView >
+         
+      )
+   }
+   navigate(itemid) {
+      if(itemid == 0)
+         Actions.StartHere();
+      if(itemid == 1)
+         Actions.Week1();
+      if(itemid == 2)
+         Actions.Week2();
+      if(itemid == 3)
+         Actions.Week3SS();
+   }
+}
+export default List
+
+
+const styles = StyleSheet.create ({
+   container: {
+      padding: 25,
+      marginTop: 5,
+      marginBottom: 5,
+      marginLeft: 5,
+      marginRight: 5,
+      backgroundColor: '#000000',
+      alignItems: 'center',
+      borderBottomWidth: 0.5,
+      borderTopWidth: 0.5,
+      borderColor: "#121212",
+      borderRadius: 15,
+   },
+   text: {
+      color: '#ffffff'
+   }
+})
+
+
+
+
+
+
+
+/*WORKS, UNDO
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, StatusBar } from 'react-native';
 import { Actions } from 'react-native-router-flux';
@@ -91,7 +223,7 @@ const styles = StyleSheet.create({
     color:"white"
   }
 });
-
+*/
 
 
 
