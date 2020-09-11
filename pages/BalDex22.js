735f97a6a79fdeb13531a15953a392ae84f0d480
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 14:19:27 -0400

    Update and rename BalDex.js to BalDex22.js

--- pages/BalDex22.js
+++ pages/BalDex22.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function BalDex(){
+export default function BalDex22(){
 
 	return(
 		<View style = {styles.container}>
