33dfe1f41cc3c57393b6f6a92f457e65946fdcd6
josheason <67991218+josheason@users.noreply.github.com>
2020-09-11 14:12:31 -0400

    Update and rename NavDeskExt.js to NavDeskExt6.js

--- pages/NavDeskExt6.js
+++ pages/NavDeskExt6.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function NavDeskExt(){
+export default function NavDeskExt6(){
 
 	return(
 		<View style = {styles.container}>
