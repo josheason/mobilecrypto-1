8f298a0d14453944a5430f07a5ca653d3ffe7ac8
josheason <67991218+josheason@users.noreply.github.com>
2020-09-15 13:01:43 -0400

    Update and rename time.js to time5.js

--- pages/Week1/time5.js
+++ pages/Week1/time5.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function time(){
+export default function time5(){
 
 	return(
 		<View style = {styles.container}>
