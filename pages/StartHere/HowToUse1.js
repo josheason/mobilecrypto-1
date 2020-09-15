3fad37971b5c946eafbdb3ec15d95cb6d8771f47
josheason <67991218+josheason@users.noreply.github.com>
2020-09-15 06:33:55 -0400

    Update HowToUse1.js

--- pages/StartHere/HowToUse1.js
+++ pages/StartHere/HowToUse1.js
@@ -4,7 +4,7 @@ import { Video } from 'expo-av';
 
 const { width, height } = Dimensions.get('window');
 
-export default function WalletDifferences2(){
+export default function HowToUse1(){
 
 	return(
 		<View style = {styles.container}>
