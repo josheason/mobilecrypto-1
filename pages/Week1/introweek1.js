cca02bb45f898e998ef84bcbb9439623d34c4fa3
josheason <67991218+josheason@users.noreply.github.com>
2020-09-17 07:56:15 -0400

    Update introweek1.js

--- pages/Week1/introweek1.js
+++ pages/Week1/introweek1.js
@@ -238,7 +238,7 @@ const styles = StyleSheet.create({
 		paddingLeft: 5,
 		//fontWeight: 'bold',
 	},
-	/*ital: {
+	ital: {
 		//flex :1,
 		backgroundColor: '#121212',
 		alignItems: 'center',
@@ -248,7 +248,7 @@ const styles = StyleSheet.create({
 		//fontWeight: 'bold',
 		fontStyle: 'italic',
 	},
-	bol: {
+	/*bol: {
 		//flex :1,
 		backgroundColor: '#121212',
 		alignItems: 'center',
