ac61a2703805a4a5aaa015b3c31b37f95b2f72cd
josheason <67991218+josheason@users.noreply.github.com>
2020-09-17 07:07:19 -0400

    Update introweek1.js

--- pages/Week1/introweek1.js
+++ pages/Week1/introweek1.js
@@ -95,7 +95,7 @@ const styles = StyleSheet.create({
 	container: {
 		//flex :1,
 		backgroundColor: '#121212',
-		alignItems: 'center',
+		//alignItems: 'center',
 		//justifyContent: 'center',
 		//justifyContent: 'space-between'
 	},
