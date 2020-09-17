18a37ef9dbae591316b584dca47dbb63409ae862
josheason <67991218+josheason@users.noreply.github.com>
2020-09-17 14:53:16 -0400

    Update rewire8.js

--- pages/Week1/rewire8.js
+++ pages/Week1/rewire8.js
@@ -15,25 +15,25 @@ const { width, height } = Dimensions.get('window');
 //export default function WhatIsDeFi1(){
 export default class rewire8 extends Component{
 	
-	/*componentDidMount(){
+	componentDidMount(){
    		const db = firebase.firestore();
    		const ud = firebase.auth().currentUser;
    		var user = firebase.auth().currentUser;
    		const increment = firebase.firestore.FieldValue.increment(1);
    		const watchedRef = db.collection('users');
    		watchedRef.where('id', '==', user.uid)
-     		 .where('introweek1', '==', false)
+     		 .where('w1_8', '==', false)
      		 .get().then(response => {
         		let batch = db.batch()
         		response.docs.forEach((doc) => {
             		const docRef = watchedRef.doc(doc.id)
-            		batch.update(docRef, {watched: increment , introweek1: true})
+            		batch.update(docRef, {watched: increment , w1_8: true})
         		})
         		batch.commit().then(() => {
             		console.log(`updated all documents inside this vid`)
         		})
     		})
-	}*/
+	}
 	
 	render(){
 	return(
