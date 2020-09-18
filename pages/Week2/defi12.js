8e96d5eb3713282929b4a3ba17336f370ce5aee0
josheason <67991218+josheason@users.noreply.github.com>
2020-09-18 17:12:09 -0400

    Update defi12.js

--- pages/Week2/defi12.js
+++ pages/Week2/defi12.js
@@ -70,7 +70,7 @@ export default class defi12 extends Component{
 					</View><></>
 				</TouchableOpacity><></>
 
-				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.sixrules7()}>
+				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.week3intro1()}>
 					<View style={styles.nexticon}>
 						<Text style={styles.buttonText}>Next</Text><></>
 						<Icon name="arrow-right" size={20} style={styles.icon}/><></>
