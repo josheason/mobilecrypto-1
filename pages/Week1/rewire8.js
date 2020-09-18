1de8daaa122a014b00eaf3d9b0807df777945aa1
josheason <67991218+josheason@users.noreply.github.com>
2020-09-18 17:10:54 -0400

    Update rewire8.js

--- pages/Week1/rewire8.js
+++ pages/Week1/rewire8.js
@@ -70,7 +70,7 @@ export default class rewire8 extends Component{
 					</View><></>
 				</TouchableOpacity><></>
 
-				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.time5()}>
+				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.week2intro1()}>
 					<View style={styles.nexticon}>
 						<Text style={styles.buttonText}>Next</Text><></>
 						<Icon name="arrow-right" size={20} style={styles.icon}/><></>
