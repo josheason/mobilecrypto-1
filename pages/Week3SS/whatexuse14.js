12e5cf5c75b71a199099d9494e70ac5f9964291d
josheason <67991218+josheason@users.noreply.github.com>
2020-09-18 09:35:11 -0400

    Update whatexuse14.js

--- pages/Week3SS/whatexuse14.js
+++ pages/Week3SS/whatexuse14.js
@@ -70,7 +70,7 @@ export default class whatexuse14 extends Component{
 					</View><></>
 				</TouchableOpacity><></>
 
-				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.bitcoin1025()}>
+				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.ultimsec15()}>
 					<View style={styles.nexticon}>
 						<Text style={styles.buttonText}>Next</Text><></>
 						<Icon name="arrow-right" size={20} style={styles.icon}/><></>
