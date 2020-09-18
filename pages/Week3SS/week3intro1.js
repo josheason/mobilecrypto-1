91e2ceb616e5fd7d4983fb4520f56884b3af3bdf
josheason <67991218+josheason@users.noreply.github.com>
2020-09-18 09:08:46 -0400

    Update week3intro1.js

--- pages/Week3SS/week3intro1.js
+++ pages/Week3SS/week3intro1.js
@@ -70,7 +70,7 @@ export default class week3intro1 extends Component{
 					</View><></>
 				</TouchableOpacity><></>
 
-				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.bitcoin1025()}>
+				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.sendcrypto2()}>
 					<View style={styles.nexticon}>
 						<Text style={styles.buttonText}>Next</Text><></>
 						<Icon name="arrow-right" size={20} style={styles.icon}/><></>
