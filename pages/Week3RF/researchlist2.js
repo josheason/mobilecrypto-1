fb7ae04c1640f5053ad8faa4ca926116702b8a18
josheason <67991218+josheason@users.noreply.github.com>
2020-09-18 10:31:03 -0400

    Update researchlist2.js

--- pages/Week3RF/researchlist2.js
+++ pages/Week3RF/researchlist2.js
@@ -70,7 +70,7 @@ export default class researchlist2 extends Component{
 					</View><></>
 				</TouchableOpacity><></>
 
-				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.chromeextwall9()}>
+				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.deskorg3()}>
 					<View style={styles.nexticon}>
 						<Text style={styles.buttonText}>Next</Text><></>
 						<Icon name="arrow-right" size={20} style={styles.icon}/><></>
