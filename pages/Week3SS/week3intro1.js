e96b4999bdd238324ca18bcd65f0272c0591642e
josheason <67991218+josheason@users.noreply.github.com>
2020-09-18 17:12:49 -0400

    Update week3intro1.js

--- pages/Week3SS/week3intro1.js
+++ pages/Week3SS/week3intro1.js
@@ -63,7 +63,7 @@ export default class week3intro1 extends Component{
 			style={styles.video}
 			/><></>
 				<View style={styles.buttonRow}>
-				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.internetwork3()}>
+				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.defi12()}>
 					<View style={styles.previcon}>
 						<Icon name="arrow-left" size={20} style={styles.icon}/><></>
 						<Text style={styles.buttonText}>Previous</Text><></>
