fe41ede8191272dbc174e7b4819f8d2b61d347fa
josheason <67991218+josheason@users.noreply.github.com>
2020-09-19 16:43:47 -0400

    Update Week2.js

--- pages/Week2/Week2.js
+++ pages/Week2/Week2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet, ScrollView } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet, ScrollView, Image } from 'react-native'
 import { Actions } from 'react-native-router-flux';
 var s = require('../styles');
    
@@ -74,9 +74,17 @@ class List extends Component {
                      key = {item.id}
                      style = {s.listcontainer}
                      onPress = {() => this.navigate(item.id)}>
-                     <Text style = {s.listtext}>
+                     <Image 
+                        source = {{uri: 'https://firebasestorage.googleapis.com/v0/b/mobilecrypto-b1543.appspot.com/o/Images%2Fthumbnail.png?alt=media&token=ae67603e-14b8-4ac2-8ca2-0209ea08e0a2'}}
+                        style = {styles.img}
+                        />
+                     <Text style = {styles.text}>
                         {item.name}
                      </Text>
+                     <Image 
+                        source = {{uri: 'https://firebasestorage.googleapis.com/v0/b/mobilecrypto-b1543.appspot.com/o/Images%2Fplaybut.png?alt=media&token=e99b2580-0f6e-465c-b0dc-10edcd4e9f70'}}
+                        style = {styles.imgR}
+                        />
                   </TouchableOpacity>
                ))
             }    
@@ -112,3 +120,27 @@ class List extends Component {
    }
 }
 export default List
+
+
+
+const styles = StyleSheet.create ({
+   text: {
+      color: '#ffffff'
+   },
+   img: {
+      width: 50,
+      height: 50,
+      position: 'absolute',
+      left: 10,
+      top: 10,
+      padding: 20,
+   },
+   imgR: {
+      width: 50,
+      height: 50,
+      position: 'absolute',
+      right: 10,
+      top: 10,
+      padding: 20,
+   },
+})
