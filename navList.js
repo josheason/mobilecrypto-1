7e6577caec2d42d4df4d0316ef2e56b42352358e
josheason <67991218+josheason@users.noreply.github.com>
2020-09-19 15:15:50 -0400

    Create navList.js

---
+++ navList.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react'
+import { Text, View, TouchableOpacity, StyleSheet, ScrollView  } from 'react-native'
+import { Actions } from 'react-native-router-flux';
+   
+class navList extends Component {
+   state = {
+      names: [
+         {
+            id: 0,
+            name: 'START HERE: How To Use & Resources',
+         },
+         {
+            id: 1,
+            name: 'Week 1: Mind',
+         },
+         {
+            id: 2,
+            name: 'Week 2: Blockchain',
+         },
+         {
+            id: 3,
+            name: 'Week 3: Security + Storage',
+         },
+         {
+            id: 4,
+            name: 'Week 3: Research Fundamentals',
+         },
+         {
+            id: 5,
+            name: 'Week 4: Marketing',
+         },
+         {
+            id: 6,
+            name: 'Week 5: Leadership',
+         },
+         {
+            id: 7,
+            name: 'Week 6: Product',
+         },
+         {
+            id: 8,
+            name: 'Week 7: Industry',
+         },
+         {
+            id: 9,
+            name: 'Week 8: Technical Analysis',
+         },
+         {
+            id: 10,
+            name: 'Week 9: Ratios',
+         },
+         {
+            id: 11,
+            name: 'Week 10: Proven Ways to Make Money In Crypto',
+         },
+         {
+            id: 12,
+            name: 'DeFi Trading',
+         },
+      ]
+   }
+   alertItemName = (item) => {
+      alert(item.name)
+   }
+   
+   render() {
+      return (
+         <ScrollView
+          style = {{ backgroundColor: '#000000' }}
+         >
+            {
+               this.state.names.map((item, index) => (
+                  <TouchableOpacity
+                     key = {item.id}
+                     style = {styles.listcontainer}
+                     onPress={()=>this.navigate(item.id)}>
+                     <Text style = {styles.text}>
+                        {item.name}
+                     </Text>
+                  </TouchableOpacity>
+               ))
+            }    
+         </ScrollView >
+         
+      )
+   }
+   navigate(itemid) {
+      if(itemid == 0)
+         Actions.Start();
+      if(itemid == 1)
+         Actions.Week1();
+      if(itemid == 2)
+         Actions.Week2();
+      if(itemid == 3)
+         Actions.Week3SS();
+      if(itemid == 4)
+         Actions.Week3RF();
+      if(itemid == 5)
+         Actions.Week4();
+      if(itemid == 6)
+         Actions.Week5();
+      if(itemid == 7)
+         Actions.Week6();
+      if(itemid == 8)
+         Actions.Week7();
+      if(itemid == 9)
+         Actions.Week8();
+      if(itemid == 10)
+         Actions.Week9();
+      if(itemid == 11)
+         Actions.Week10();
+      if(itemid == 12)
+         Actions.DeFi();
+   }
+}
+export default navList
+
+
+
+styles = StyleSheet.create({
+
+listcontainer: {
+    padding: 25,
+    marginTop: 5,
+    marginBottom: 5,
+    marginLeft: 5,
+    marginRight: 5,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    borderColor: "#121212",
+    borderWidth: 1,
+},
+
+listtext:{
+  color: '#ffffff',
+  textAlign: 'center',
+},
+
+});
+
+
