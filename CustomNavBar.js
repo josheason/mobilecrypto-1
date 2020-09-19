b9fa97492e8a09f4519dd73f68acdefcdd5c7f22
josheason <67991218+josheason@users.noreply.github.com>
2020-09-19 12:37:14 -0400

    Update CustomNavBar.js

--- CustomNavBar.js
+++ CustomNavBar.js
@@ -16,6 +16,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        alignSelf:'center',
+        flexDirection: 'column',
     },
 });
 
