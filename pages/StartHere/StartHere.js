f14b58cd38f357eecf39221c04738dbc2abb749f
josheason <67991218+josheason@users.noreply.github.com>
2020-09-19 07:48:00 -0400

    Update StartHere.js

--- pages/StartHere/StartHere.js
+++ pages/StartHere/StartHere.js
@@ -12,18 +12,54 @@ class List extends Component {
          },
          {
             id: 1,
-            name: 'Scam Submitter',
+            name: 'Essential Reading List',
          },
-         {
+          {
             id: 2,
             name: 'Essential Reading List',
          },
-         {
+          {
             id: 3,
+            name: 'Essential Reading List',
+         },
+          {
+            id: 4,
+            name: 'Essential Reading List',
+         },
+          {
+            id: 5,
+            name: 'Essential Reading List',
+         },
+          {
+            id: 6,
+            name: 'Essential Reading List',
+         },
+          {
+            id: 7,
+            name: 'Essential Reading List',
+         },
+         {
+            id: 8,
             name: 'Bookmarks',
          },
          {
-            id: 4,
+            id: 9,
+            name: 'Bookmarks',
+         },
+         {
+            id: 10,
+            name: 'Bookmarks',
+         },
+         {
+            id: 11,
+            name: 'Bookmarks',
+         },
+         {
+            id: 12,
+            name: 'Bookmarks',
+         },
+         {
+            id: 13,
             name: 'Assessment',
          },
       ]
@@ -41,7 +77,7 @@ class List extends Component {
                   <TouchableOpacity
                      key = {item.id}
                      style = {s.listcontainer}
-                     onPress = {() => this.alertItemName(item)}>
+                     onPress = {() => this.navigate(item.id)}>
                      {/* onPress={()=> this.navigate() }> */}
                     {/* onPress={()=>this.navigate(item.id)}> */}
 
@@ -58,9 +94,31 @@ class List extends Component {
    }
    navigate(itemid) {
       if(itemid == 0)
-         Actions.home();
+         Actions.HowToUse1 ();
       if(itemid == 1)
-         this.navigate2();
+         Actions.EssentialReadlingList3b();
+      if(itemid == 2)
+         Actions.EssentialReadlingList3p();
+      if(itemid == 3)
+         Actions.EssentialReadlingList3i();
+      if(itemid == 4)
+         Actions.EssentialReadlingList3br();
+      if(itemid == 5)
+         Actions.EssentialReadlingList3c();
+      if(itemid == 6)
+         Actions.EssentialReadlingList3m();
+      if(itemid == 7)
+         Actions.Bookmarks4c();
+      if(itemid == 8)
+         Actions.Bookmarks4d();
+      if(itemid == 9)
+         Actions.Bookmarks4m();
+      if(itemid == 10)
+         Actions.Bookmarks4mac();
+      if(itemid == 11)
+         Actions.Bookmarks4mar();
+      if(itemid == 12)
+         Actions.home();
    }
 }
 export default List
