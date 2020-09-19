ddd63eefe6674b22dcc57f8d59d1c264b04c0dc7
josheason <67991218+josheason@users.noreply.github.com>
2020-09-19 07:06:46 -0400

    Create EssentialReadingList3i.js

---
+++ pages/StartHere/EssentialReadingList3i.js
@@ -0,0 +1,282 @@
+//import React from 'react';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Dimensions, Linking, ScrollView, Button, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { Video } from 'expo-av';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Actions } from 'react-native-router-flux';
+
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+const { width, height } = Dimensions.get('window');
+
+
+ 
+export default class EssentialReadingList3i extends Component{
+	
+	
+	render(){
+	return(
+		<View style = {styles.container}>
+			<Text style = {styles.resour1}>{`Essential Reading List`}</Text>
+			    <Text style = {styles.para}>{`Investing`}</Text>
+ 			   <Text style = {styles.para} onPress={() => Linking.openURL('https://amzn.to/2RBvZZ4')}>
+ 			     <Text style = {styles.reg}>{`\u2022  `}</Text>
+  			    <Text style={styles.regURL} >{'Principles'}</Text>
+ 			   </Text>
+          <Text style = {styles.para} onPress={() => Linking.openURL('https://amzn.to/2yhHQog')}>
+ 			     <Text style = {styles.reg}>{`\u2022  `}</Text>
+  			    <Text style={styles.regURL} >{'Rich Dad Poor Dad'}</Text>
+ 			   </Text>
+			    <Text style = {styles.para} onPress={() => Linking.openURL('https://amzn.to/2ygaCFZ')}>
+ 			     <Text style = {styles.reg}>{`\u2022  `}</Text>
+ 			     <Text style={styles.regURL} >{`The Richest Man in Babylon`}</Text>
+			    </Text>
+			    <Text style = {styles.para}  onPress={() => Linking.openURL('https://amzn.to/2ymP8Hb')}>
+			      <Text style = {styles.reg}>{`\u2022  `}</Text>
+			      <Text style={styles.regURL}>{'The Intelligent Investor'}</Text>
+  			  </Text>
+				<Text style= {styles.lastPara}>{`   `}</Text>
+					<View style={styles.buttonRow}>
+				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.week2intro1()}>
+					<View style={styles.previcon}>
+						<Icon name="arrow-left" size={20} style={styles.icon}/>
+						<Text style={styles.buttonText}>Previous</Text>
+					</View>
+				</TouchableOpacity>
+				<TouchableOpacity activeOpacity={0.3} underlayColor="#6200ee" onPress={() => Actions.internetwork3()}>
+					<View style={styles.nexticon}>
+						<Text style={styles.buttonText}>Next</Text>
+						<Icon name="arrow-right" size={20} style={styles.icon}/>
+					</View>
+				</TouchableOpacity>
+			</View>
+		</View>
+		);
+}
+}
+
+	
+const styles = StyleSheet.create({
+	video: {
+		width: width,
+		height: height/ 3,
+		alignItems: 'center'
+	},
+	space: {
+		width: width,
+		height: height/ 10,
+		alignItems: 'center'
+	},
+	poster: {
+		width: width,
+		height: height/ 3,
+		opacity: 0.5,
+	},
+	container: {
+		flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+	},
+	containerURL: {
+		//flex :1,
+		backgroundColor: '#121212',
+		color: '#03dac5',
+		alignItems: 'center',
+		paddingLeft: 5,
+	},
+	paraURL: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#03dac5',
+		fontSize: 15,
+		//margin: 20,
+		marginTop: 20,
+		marginLeft: 5,
+		//fontWeight: 'bold',
+	},
+	text: {
+		///flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 30,
+		fontWeight: 'bold',
+		position: 'absolute',
+		top: 15,
+		paddingLeft: 5,
+	},
+	resour: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 25,
+		fontWeight: 'bold',
+		//margin: 20,
+		//paddingTop: 20,
+		paddingBottom: 15,
+		paddingLeft: 5,
+	},
+	resour1: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 25,
+		fontWeight: 'bold',
+		//margin: 20,
+		//paddingTop: 20,
+		paddingBottom: 30,
+		paddingLeft: 5,
+	},
+	firstpara: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 15,
+		//margin: 20,
+		paddingTop: 20,
+		paddingLeft: 5,
+		fontWeight: 'bold',
+	},
+	para: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 20,
+		//margin: 20,
+		//paddingRight: 5,
+		marginTop: 25,
+		marginBottom: 25,
+		//paddingLeft: 5,
+		fontWeight: 'bold',
+	},
+	ital: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 15,
+		//fontWeight: 'bold',
+		fontStyle: 'italic',
+	},
+	reg: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 15,
+		//fontWeight: 'bold',
+		//fontStyle: 'italic',
+	},
+  	regURL: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#03dac5',
+		fontSize: 18,
+	  	marginTop: 15,
+		//padding:20,
+		//fontWeight: 'bold',
+		//fontStyle: 'italic',
+	},
+	bol: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 15,
+		fontWeight: 'bold',
+		//fontStyle: 'italic',
+	},
+	bolPara: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 15,
+		//margin: 20,
+		paddingTop: 20,
+		paddingLeft: 5,
+		fontWeight: 'bold',
+	},
+	italPara: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 15,
+		//margin: 20,
+		paddingTop: 20,
+		paddingLeft: 5,
+		fontStyle: 'italic',
+	},
+	lastPara: {
+		//flex :1,
+		backgroundColor: '#121212',
+		alignItems: 'center',
+		//justifyContent: 'center',
+		color: '#fff',
+		fontSize: 15,
+		//margin: 20,
+		paddingTop: 20,
+		paddingLeft: 5,
+		paddingBottom: 50,
+		//fontWeight: 'bold',
+	},
+	prevbutton: {
+		flex: 1,
+    	justifyContent: "center",
+    	padding: 16
+	},
+	buttonText:{
+		fontSize: 21,
+		color: '#fff',
+		alignItems: 'center'
+	},
+	previcon: {
+		color:"#fff",
+		backgroundColor: '#6200ee',
+		padding: 5,
+		flexDirection: 'row',
+		justifyContent: 'center',
+		width: width/2,
+	},
+	nexticon: {
+		color:"#fff",
+		backgroundColor: '#3700b3',
+		padding: 5,
+		flexDirection: 'row',
+		width: width/2,
+		justifyContent: 'center',
+	},
+	icon: {
+		color:"#fff",
+		padding: 5,
+	},
+	buttonRow:{
+		flexDirection: 'row',
+		marginTop: 5,
+		marginBottom: 20,
+	},
+});
+
+
+
