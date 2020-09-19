1aad579636365c57bfd5e037b1185e7004a74933
josheason <67991218+josheason@users.noreply.github.com>
2020-09-19 07:56:23 -0400

    Update EssentialReadingList3m.js

--- pages/StartHere/EssentialReadingList3m.js
+++ pages/StartHere/EssentialReadingList3m.js
@@ -22,7 +22,7 @@ export default class EssentialReadingList3m extends Component{
 			    <Text style = {styles.para}>{`Business`}</Text>
  			   <Text style = {styles.para} onPress={() => Linking.openURL('https://amzn.to/3a8e0Qq')}>
  			     <Text style = {styles.reg}>{`\u2022  `}</Text>
-  			    <Text style={styles.regURL} >{'Trust Me I'm Lying '}</Text>
+  			    <Text style={styles.regURL} >{`Trust Me I'm Lying '}</Text>
  			   </Text>
           <Text style = {styles.para} onPress={() => Linking.openURL('https://amzn.to/2Vzvq2V')}>
  			     <Text style = {styles.reg}>{`\u2022  `}</Text>
