ae767afc7427c24d7f45137f36313985280b56fa
josheason <67991218+josheason@users.noreply.github.com>
2020-09-19 23:58:49 -0400

    Create Progress.js

---
+++ pages/Progress.js
@@ -0,0 +1,82 @@
+import React, {useRef, useState, useEffect} from 'react';
+import { Text, View, StyleSheet, Animated } from 'react-native';
+import Constants from 'expo-constants';
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+
+const Progress = () => {
+  let animation = useRef(new Animated.Value(0));
+  const [progress, setProgress] = useState(0);
+  useInterval(() => {
+      setProgress(5);
+  }, 1000);
+
+  useEffect(() => {
+    Animated.timing(animation.current, {
+      toValue: progress,
+      duration: 100
+    }).start();
+  },[progress])
+
+  const width = animation.current.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+    extrapolate: "clamp"
+  })
+  return (
+    <View style={styles.container1}>
+      <Text>
+        Loading…..
+      </Text>
+      <View style={styles.progressBar}>
+        <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: "#8BED4F", width }}/>
+      </View>
+      <Text>
+        {`${progress}%`}
+      </Text>
+
+    </View>
+  );
+}
+
+export default Progress;
+
+const styles = StyleSheet.create({
+  container1: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  progressBar: {
+    flexDirection: 'row',
+    height: 20,
+    width: '100%',
+    backgroundColor: 'white',
+    borderColor: '#000',
+    borderWidth: 2,
+    borderRadius: 5
+  }
+});
